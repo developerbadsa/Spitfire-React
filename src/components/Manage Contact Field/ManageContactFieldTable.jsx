d6d7933cf7edb0d57b522d0b191e67b4c5af29ca
Rahim_Badsa <rahimbadsa723@gmail.com>
2024-10-25 20:22:35 +0600

    ..

--- src/components/Manage Contact Field/ManageContactFieldTable.jsx
+++ src/components/Manage Contact Field/ManageContactFieldTable.jsx
@@ -1,6 +1,8 @@
 import {DocumentIcon} from '@heroicons/react/24/solid';
 import {ArrowDownTrayIcon} from '@heroicons/react/24/outline';
 import {Button, Card, IconButton, Typography} from '@material-tailwind/react';
+import {FaRegEdit} from 'react-icons/fa';
+import {IoMdCloseCircleOutline} from 'react-icons/io';
 
 const TABLE_HEAD = ['Name', 'Field Type', 'Action'];
 
@@ -13,14 +15,20 @@ const TABLE_ROWS = [
    {
       name: 'Dulufa',
       FieldType: 'Hi Swqure',
-      Action: 'Default',
+      Action: 'Defaults',
    },
 ];
 
 export function ManageContactFieldTable() {
    return (
       <Card className='h-full w-[85%] overflow-hidden mx-auto '>
-         <div className=' text-end'><Button size="sm" className='bg-[#ed9034] py-3 mr-6 rounded-lg mt-4'>+ Create a Field</Button></div>
+         <div className=' text-end'>
+            <Button
+               size='sm'
+               className='bg-[#ed9034] py-3 mr-6 rounded-lg mt-4'>
+               + Create a Field
+            </Button>
+         </div>
          <table className='w-full min-w-max table-auto text-left'>
             <thead>
                <tr>
@@ -45,7 +53,10 @@ export function ManageContactFieldTable() {
                               variant='small'
                               color='blue-gray'
                               className='font-bold'>
-                              {name}
+
+
+                              {Action == 'Default' ? name
+                                 : `<span>${name}<span/>`}
                            </Typography>
                         </td>
                         <td className='p-4'>
@@ -60,14 +71,17 @@ export function ManageContactFieldTable() {
                               'Default'
                            ) : (
                               <div className='flex items-center gap-2'>
-                                 <IconButton variant='text' size='sm'>
-                                    <DocumentIcon className='h-4 w-4 text-gray-900' />
+                                 <IconButton
+                                    variant='text'
+                                    className='bg-[#ed9034] hover:bg-[#bc7a38] rounded-full text-white'
+                                    size='sm'>
+                                    <FaRegEdit />
                                  </IconButton>
-                                 <IconButton variant='text' size='sm'>
-                                    <ArrowDownTrayIcon
-                                       strokeWidth={3}
-                                       className='h-4 w-4 text-gray-900'
-                                    />
+                                 <IconButton
+                                    variant='text'
+                                    className='bg-[#f1416c] hover:bg-[#b03453] rounded-full text-white'
+                                    size='sm'>
+                                    <IoMdCloseCircleOutline />
                                  </IconButton>
                               </div>
                            )}
